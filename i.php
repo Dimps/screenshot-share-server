3c124ae5a59a47268af9d51c110fa4d6a65b3420
Dima Stefantsov <dima@stefantsov.com>
2015-06-29 19:54:39 +0300

    initial

---
+++ i.php
@@ -0,0 +1,16 @@
+<?php
+
+if (file_exists("i.png")) {
+    $name = date("Y.m.d_G.i.s_", strtotime("+3 hours")) . substr(str_shuffle(str_repeat("0123456789abcdefghijklmnopqrstuvwxyz", 8)), 0, 8) . ".png";
+    $success = rename("i.png", $name);
+    
+    if ($success) {
+        echo "http://4.stefantsov.com/s/", $name;
+    } else {
+        echo "couldn't rename";
+    }
+} else {
+    echo "failed";
+}
+
+?>
